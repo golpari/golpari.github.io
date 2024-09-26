92b903687d3fc7a252ec43d2a906d793599424dc
golpari <golpari@umich.edu>
2024-09-26 17:37:18 -0400

    added more transitions for header scene, including learn more button and subtitle animations

--- wip-index.js
+++ wip-index.js
@@ -1,31 +1,71 @@
-// use a script tag or an external JS file
+// Use a script tag or an external JS file
 // Apply GSAP animation to all images within the container
 document.addEventListener("DOMContentLoaded", (event) => {
-    gsap.registerPlugin(ScrollTrigger)
-    // gsap code here!
+    gsap.registerPlugin(ScrollTrigger);
+
     // Create a master timeline to control the sequence of animations
     const tl = gsap.timeline({
         scrollTrigger: {
             trigger: '.container',
             start: 'top top',
-            end: '+=100%', // Adjust based on the required animation length
+            end: "+=" + window.innerHeight, // Adjust based on the required animation length
             pin: true,
             scrub: true,
-            // markers: true, // Uncomment for debugging markers
-        }
+            markers: true, // Uncomment for debugging markers
+        },
     });
 
-    // First animation: img-1 zooms in
+    // First animation: img-2 zooms in
     tl.to('#img-2', {
         scale: 1.5,
-        ease: "none",
-        duration: 3
+        ease: 'none',
+        duration: 3,
     });
 
-    // Second animation: img-2 zooms in after img-1
+    // Fade text at the same time the img2 animation happens
+    tl.to(
+        '.header-text h1',
+        {
+            opacity: 0,
+            duration: 3,
+            y: 10,
+        }, 0 // this last part marks a delay, this has 0 delay so it happens at the same time as the #img-2 animation
+    );
+
+    //move up the subtitles texts to the center of the screen as name fades out
+    tl.to(
+        '.header-text .subtitle',
+        {
+            duration: 3,
+            y: -100,
+        } // this last part marks a delay
+    );
+
+    // fade out the main subtitle (eg: 'UI Engineer')
+    tl.to(
+        '.header-text .subtitle',
+        {
+            opacity: 0,
+            duration: 3,
+            y: -100,
+        }, 5 // this last part marks a delay
+    );
+
+    // Second animation: img-1 zooms in after img-2
     tl.to('#img-1', {
         scale: 1.5,
-        ease: "none",
-        duration: 3
-    });
+        ease: 'none',
+        duration: 3,
+    }); 
+
+    // fade IN the button to go to the next section
+    tl.to(
+        '.header-text .button',
+        {
+            opacity: 1,
+            duration: 2,
+            y: -20,
+        }, "<");
+
+    
 });
